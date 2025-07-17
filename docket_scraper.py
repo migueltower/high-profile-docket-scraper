38ebdb253d49a62c4844512bf505d8df370e4583
migueltower <106282745+migueltower@users.noreply.github.com>
2025-07-17 13:49:15 -0700

    Update docket_scraper.py

--- docket_scraper.py
+++ docket_scraper.py
@@ -1,6 +1,14 @@
 import os
 import time
 import logging
+import sys
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+logging.info("📢 Scraper started")
+
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
